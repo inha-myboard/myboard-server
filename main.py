e62f9d24feeddca743a4d85d4f5406abeb68ccd0
bdh92123 <bdh92123.1@gmail.com>
2017-10-09 18:16:25 +0900

    Add config

--- main.py
+++ main.py
@@ -12,11 +12,9 @@ def test(name = None):
 	return(render_template(name))
 
 if __name__ == "__main__":
-	#MySQL configurations
-	app.config['MYSQL_DATABASE_USER'] = 'root'
-	app.config['MYSQL_DATABASE_PASSWORD'] = '1234'
-	app.config['MYSQL_DATABASE_DB'] = 'mydb'
-	app.config['MYSQL_DATABASE_HOST'] = '127.0.0.1'
+	app.config.from_object('myboard.default_settings')
+	app.config.from_envvar('MYBOARD_SETTINGS')
+
 	mysql.init_app(app)
 	mysql.connect()
 
---
+++ myboard
@@ -0,0 +1,2 @@
+export MYBOARD_SETTINGS=`pwd`/$1.cfg
+python main.py 
