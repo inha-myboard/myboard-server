aad5c24cba36603ecd2c9fb563292be08576ad22
Jehyung Lee <trevor91@naver.com>
2017-10-17 16:40:08 +0900

    API (using selenium)

--- APIv1.0.py
+++ APIv1.0.py
@@ -1,22 +1,16 @@
-import requests
-from bs4 import BeautifulSoup
-import re
+from selenium import webdriver
+import time
+import json
+inputjson = """{"type":"static","body_selector":"html > body > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div > ul > li","segments":[{"selector":"a > span:nth-child(1)","name":"rank"},{"selector":"a > span:nth-child(2)","name":"name"}]}"""
+data = json.loads(inputjson)
+url = 'https://www.naver.com/'
 
+driver = webdriver.PhantomJS('./PhantomJS.exe')
+driver.get(url)
+body = driver.find_element_by_css_selector(data['body_selector'])
 
-source = requests.get("http://www.naver.com")
-html = source.text
-soup = BeautifulSoup(html, "html.parser")
-# bodyselector = "html > body > div > div > div > div > div > ul > li"
-bodyselector = "html > body > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(3) > ul:nth-child(4) > li"
-
-# cont = soup.select(bodyselector)
-cont = soup.select(bodyselector)
-# print(cont)
-
-cont = re.sub("<.*?>","",str(cont))
-print(cont)
-
-
-
-
-# {"type":"static","body_selector":"","segments":[{"selector":"a:nth-child(1) > span:nth-child(1)","name":"rank"},{"selector":"a:nth-child(1) > span:nth-child(2)","name":"cont"}]}
+rst = list()
+for i in range(len(data["segments"])):
+	rst.append(body.find_element_by_css_selector(data["segments"][i]['selector']).text)
+print(rst)
+#DB, flask
